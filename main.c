dfd0972af6ca874d65e00acc1fd9b83e93e94bfa
JFreud <jrm.frdnbrg@gmail.com>
2018-03-02 14:05:31 -0500

    worked

--- main.c
+++ main.c
@@ -25,28 +25,30 @@ int main(int argc, char **argv) {
     parse_file( "stdin", transform, edges, s );
 
 
-  c.red = 139;
-  c.green = 69;
-  c.blue = 19;
-  add_edge(edges, 100, 175, 0, 100, 375, 0);
-  add_edge(edges, 400, 175, 0, 400, 375, 0);
-  add_edge(edges, 100, 175, 0, 250, 100, 0);
-  add_edge(edges, 400, 175, 0, 250, 100, 0);
-  add_edge(edges, 250, 100, 0, 250, 300, 0);
-
-  matrix_mult(make_translate(100, -50, 30), transform);
-
-  matrix_mult(make_scale(0.5, 0.5, 0.5), transform);
-
-  matrix_mult(make_rotZ(45), transform);
-
-  
-
-  print_matrix(transform);
-  matrix_mult(transform, edges);
-  draw_lines(edges, s, c);
-  display(s);
-  save_extension(s, "result.png");
+  // c.red = 139;
+  // c.green = 69;
+  // c.blue = 19;
+  // add_edge(edges, 100, 175, 0, 100, 375, 0);
+  // add_edge(edges, 400, 175, 0, 400, 375, 0);
+  // add_edge(edges, 100, 175, 0, 250, 100, 0);
+  // add_edge(edges, 400, 175, 0, 250, 100, 0);
+  // add_edge(edges, 250, 100, 0, 250, 300, 0);
+  //
+  // matrix_mult(make_translate(100, -50, 30), transform);
+  //
+  // matrix_mult(make_scale(0.5, 0.5, 0.5), transform);
+  //
+  // matrix_mult(make_rotZ(10), transform);
+  // matrix_mult(make_rotX(45), transform);
+  // matrix_mult(make_rotY(30), transform);
+  //
+  //
+  //
+  // print_matrix(transform);
+  // matrix_mult(transform, edges);
+  // draw_lines(edges, s, c);
+  // display(s);
+  // save_extension(s, "result.png");
 
   free_matrix( edges );
   free_matrix( transform );
--- parser.c
+++ parser.c
@@ -11,30 +11,30 @@
 
 
 /*======== void parse_file () ==========
-Inputs:   char * filename 
-          struct matrix * transform, 
+Inputs:   char * filename
+          struct matrix * transform,
           struct matrix * pm,
           screen s
-Returns: 
+Returns:
 
 Goes through the file named filename and performs all of the actions listed in that file.
 The file follows the following format:
      Every command is a single character that takes up a line
      Any command that requires arguments must have those arguments in the second line.
      The commands are as follows:
-         line: add a line to the edge matrix - 
+   line: add a line to the edge matrix -
 	    takes 6 arguemnts (x0, y0, z0, x1, y1, z1)
-	 ident: set the transform matrix to the identity matrix - 
-	 scale: create a scale matrix, 
-	    then multiply the transform matrix by the scale matrix - 
+	 ident: set the transform matrix to the identity matrix -
+	 scale: create a scale matrix,
+	    then multiply the transform matrix by the scale matrix -
 	    takes 3 arguments (sx, sy, sz)
-	 translate: create a translation matrix, 
-	    then multiply the transform matrix by the translation matrix - 
+	 translate: create a translation matrix,
+	    then multiply the transform matrix by the translation matrix -
 	    takes 3 arguments (tx, ty, tz)
 	 rotate: create an rotation matrix,
 	    then multiply the transform matrix by the rotation matrix -
 	    takes 2 arguments (axis, theta) axis should be x y or z
-	 apply: apply the current transformation matrix to the 
+	 apply: apply the current transformation matrix to the
 	    edge matrix
 	 display: draw the lines of the edge matrix to the screen
 	    display the screen
@@ -52,8 +52,8 @@ humans use degrees, so the file will contain degrees for rotations,
 be sure to conver those degrees to radians (M_PI is the constant
 for PI)
 ====================*/
-void parse_file ( char * filename, 
-                  struct matrix * transform, 
+void parse_file ( char * filename,
+                  struct matrix * transform,
                   struct matrix * edges,
                   screen s) {
 
@@ -61,14 +61,75 @@ void parse_file ( char * filename,
   char line[256];
   clear_screen(s);
 
-  if ( strcmp(filename, "stdin") == 0 ) 
+
+  if ( strcmp(filename, "stdin") == 0 )
     f = stdin;
   else
     f = fopen(filename, "r");
-  
-  while ( fgets(line, 255, f) != NULL ) {
+
+  while (fgets(line, 255, f) != NULL ) {
     line[strlen(line)-1]='\0';
     printf(":%s:\n",line);
+
+    if (strcmp(line, "line") == 0) {
+      double x0, x1, y0, y1, z0, z1;
+      fgets(line, 255, f);
+      sscanf(line, "%lf %lf %lf %lf %lf", &x0, &y0, &z0, &x1, &y1, &z1);
+      add_edge(edges, x0, y0, z0, x1, y1, z1);
+    }
+
+    else if (strcmp(line, "ident") == 0) {
+      ident(transform);
+    }
+
+    else if (strcmp(line, "scale") == 0) {
+      double sx, sy, sz;
+      fgets(line, 255, f);
+      sscanf(line, "%lf %lf %lf", &sx, &sy, &sz);
+      matrix_mult(make_scale(sx, sy, sz), transform);
+    }
+
+    else if(strcmp(line, "translate") == 0) {
+      double tx, ty, tz;
+      fgets(line, 255, f);
+      sscanf(line, "%lf %lf %lf", &tx, &ty, &tz);
+      matrix_mult(make_translate(tx, ty, tz), transform);
+    }
+
+    else if(strcmp(line, "rotate") == 0) {
+      double theta;
+      char axis;
+      fgets(line, 255, f);
+      sscanf(line, "%c %lf", &axis, &theta);
+      if (axis == "x") {
+        matrix_mult(make_rotX(theta), transform);
+      }
+      else if (axis == "y") {
+        matrix_mult(make_rotY(theta), transform);
+      }
+      else if (axis == "z") {
+        matrix_mult(make_rotZ(theta), transform);
+      }
+      else {
+        printf("Ya dun goof. No axis called %c...\n", axis);
+        exit(0);
+      }
+    }
+
+    else if(strcmp(line, "apply") == 0) {
+      
+    }
+
+    else if(strcmp(line, "display") == 0) {
+    }
+
+    else if(strcmp(line, "save") == 0) {
+
+    }
+
+    else if(strcmp(line, "quit") == 0) {
+
+    }
+
   }
 }
-  
